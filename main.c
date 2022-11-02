30e8ace9865e5894cf4483b2977d71d6cb7a05de
sluk267 <116813345+sluk267@users.noreply.github.com>
2022-11-02 08:46:00 -0400

    update:
    optimization Update

--- main.c
+++ main.c
@@ -2,24 +2,30 @@
 #include <math.h>
 
 void fun (int x){
-printf("Hello");
-return;
-}
+    printf("Hello");
+    return;
+    }
 
 int add (int num1, int num2){
     int sum = num1+num2;
     return;
+    }
+int runadd (){
+    int a, num1,num2;
+
+    printf("\nenter number 1: ");
+    scanf("%d", &num1);
+    printf("\nEnter number 2: ");
+    scanf("%d", &num2);
+    a = add(num1,num2);
+    printf("\nbig boiii: %d\n", a);
+    return;
+
 }
 
 int main(){
-int x,a, num1,num2;
-fun(x); 
+    int a, num1,num2;
 
-printf("\nenter number 1: ");
-scanf("%d", &num1);
-printf("\nEnter number 2: ");
-scanf("%d", &num2);
-a = add(num1,num2);
-printf("\n%d\n", a);
+    runadd();
 
 }
